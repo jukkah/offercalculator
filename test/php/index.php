099d55b9c59e883db596ad4e3242f09f8c631c22
Jukka Hyytiälä <hyytiala.jukka@gmail.com>
2012-07-04 00:36:57 +0300

    Siirrytty Zendiin

--- test/php/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace test;
-
-include("EnhanceTestFramework.php");
-// Find the tests - '.' is the current folder
-\Enhance\Core::discoverTests('.');
-// Run the tests
-\Enhance\Core::runTests();
