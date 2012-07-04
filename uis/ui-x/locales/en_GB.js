28c51e53d50d2ff1e8d0cd7828b7950c1bbb94a1
Petja <petja@petjatouru.com>
2012-07-04 08:45:11 -0700

    Ja sitten takaisin ui-x:ksi!

---
+++ uis/ui-x/locales/en_GB.js
@@ -0,0 +1,30 @@
+
+// 2012 Petja Touru
+// Licensed as CC-BY-SA
+
+localization =
+{
+	"ProjectFullName"				:	"Offercalculator",
+	"MySettings"					:	"My Settings",
+	"LogOut"						:	"Log out",
+	"UserName"						:	"Username",
+	"ProductionCause"				:	"Production",
+	"RawMaterial"					:	"Raw material",
+	"Jobs"							:	"Jobs",
+	"Tools"							:	"Tools",
+	"Products"						:	"Products",
+	"ChangeLanguage"				:	"Change language",
+	"StandardProducts"				:	"Standard products",
+	"ComplexProducts"				:	"Complex products",
+	"Calculations"					:	"Calculations",
+	"NormalCalculations"			:	"Normal offercalculations",
+	"Other"							:	"Other",
+	"CalculationBases"				:	"Calculation bases",
+	"LogIn"							:	"Login",
+	"Register"						:	"Register",
+	"PasswordReset"					:	"Reset password",
+	"Password"						:	"Password",
+	"LogButton"						:	"Log",
+	"Authors"						:	"Authors",
+	"AuthorsParagraph"				:	"The implementation of the project have participated in"
+};
